ae8ac77756421d007e98b8c1da6f8b5370529b74
jian-13 <d2530857159@163.com>
2021-04-15 20:20:00 +0800

    第一个idea的项目上传到git

--- src/java/day01/Demotext.java
+++ src/java/day01/Demotext.java
@@ -3,5 +3,6 @@ package day01;
 public class Demotext {
     public static void main(String[] args) {
         System.out.println("这是idea的第一个项目");
+        System.out.println("这是idea的第一个项目");
     }
 }
