dffc4aef687ea1a242f05f918227b7520d5a38f2
wzq0519 <82557313+wzq0519@users.noreply.github.com>
2021-05-20 13:57:04 +0800

    Update Demotext3.java

--- src/java/day01/Demotext3.java
+++ src/java/day01/Demotext3.java
@@ -4,5 +4,6 @@ public class Demotext3 {
     public static void main(String[] args) {
         System.out.println("主节点操作");
         System.out.println("dddddd");
+        System.out.println("2021年5月20日，到此一游")
     }
 }
